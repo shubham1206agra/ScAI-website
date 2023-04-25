2e1318ee6f1251a2d99aa6bdf0deebc9496cb885
Mohit Kataria <mohit.kataria8860@gmail.com>
2023-04-25 12:20:48 +0530

    Update Data.js

--- textComp/join/mtech/Data.js
+++ textComp/join/mtech/Data.js
@@ -29,33 +29,39 @@ const Data = () => {
                         could be from course buckets such as mathematics,
                         learning, data science, and applications.
                     </p>
-                    <p>In the long run, ScAI also envisions starting an Executive 
-                        Masters programme taught primarily via evening and weekend classes. 
-                        This programme will cater to the growing global demand by training 
-                        industry professionals in modern AI techniques. Please check back 
-                        this space later for any updates.
-                    </p>
-                    <h2>Eligibility</h2>
-                    <p>Four Year B.Tech. / B.E. Degree (first class) or any equivalent
-                        degree (e.g., B.S. from a foreign university, and  M.Sc., MCA, or 
-                        other Master’s degrees done as part of a 3+2 model) from any Engineering or Science discipline. 
-                        A valid GATE score would be needed where such a requirement is not waived, such as students from 
-                        an IIT with a CGPA exceeding a threshold. Strong interest and background in computational mathematics and programming are preferred.
-                    </p>
-                    <h2>Admissions New Update</h2>
-                    <p>The test and interviews for M.Tech/MSR/Ph.D. admissions will be conducted online. The exact date will be announced on very short notice. 
-                    Please keep checking the website and your email.
-                    </p>
-
-                     <p>The syllabus of the test:
+                    <h2>Admission process:</h2>
+                    <p>The admission process for M.Tech consist of two stages:
                          <ul>
-                            <li>Basic maths (Linear Algebra, Probability, and Statistics)</li>
-                            <li>Aptitude</li>
-                            <li>Basics of AI/ML</li>
-                            <li>Programming and Data structure.</li>
-                          </ul>
+                            <li>In the first stage, the candidates are short 
+                            listed based on pre-approved shortlisting criteria set by the School.
+                            Note that shortlisting criteria of the school is more stringent
+                            that the minimum shortlisting criteria set by the institute. For more information,
+                            please refer to the previous year shortlisting criteria. 
+                            The criteria may change semester to semester but can give
+                            you a broad idea about the expectations. Four Year B.Tech. / B.E. Degree (first class)
+                            or any equivalent degree (e.g., B.S. from a foreign university,
+                            and M.Sc., MCA, or other Master’s degrees done as part of a 3+2 model)
+                            from any Engineering or Science discipline. A valid GATE score would be
+                            needed where such a requirement is not waived, such as students from an
+                            IIT with a CGPA exceeding a threshold. Strong interest and background in
+                            computational mathematics and programming are preferred.</li>
+                            <li>In the second stage, the shortlisted candidates are invited to appear
+                            for an online written test. The written test has five sections: (a) General Aptitude,
+                            (b) Maths and Stats, (c) Data Structure and Algorithm, (d) Machine Learning, and
+                            (e) Programming. Note that the marks in 3 best sections are considered for calculating
+                            the marks. Also, programming section has more weightage as compared to the other section.
+                            The syllabus and reference materials for the written exam can be 
+                            <a href="put_link" target="_blank" rel="noopener noreferrer" >found here</a>.
+                            Some sample questions are also provided. The final offers will be made based on GATE
+                            SCORE/CGPA and the written performance having 70:30 weightage ratio.</li>
+                        </ul>
+                    See <a href="put_link" target="_blank" rel="noopener noreferrer" >admission link</a> for further details.
                     </p>
-                  
+                    <h2>Course Structure:</h2>
+                    <p>Every MINDS candidate at Yardi ScAI has to complete minimum 50 credits of coursework while touching at least three buckets.
+                    Please check course <a href="put_link" target="_blank" rel="noopener noreferrer" >bucket list</a>
+                    for further details. Note that the bucket list gets updated periodically.
+                    </p>
                     <p>M.Tech/MSR Shortlisting Criteria : {" "}<a href="/pdf/MSR_MTech_Shortlisting_Criteria_2022.pdf" target="_blank" rel="noopener noreferrer" >click here</a>
                     </p>
                     <p>If you want to view the information brochure, please <a href = "https://owncloud.iitd.ac.in/nextcloud/index.php/s/D9ymD3jboaD5JBt">click here</a>.
