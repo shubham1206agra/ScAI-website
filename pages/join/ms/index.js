ddd601449e1ba2d30c590bc01bdaa6ae9a74f54d
Mohit Kataria <mohit.kataria8860@gmail.com>
2023-04-25 13:00:51 +0530

    Update index.js

--- pages/join/ms/index.js
+++ pages/join/ms/index.js
@@ -1,1 +1,26 @@
+import React from "react";
+import StartTop from "../../../components/StartTop/StartTop";
+import Waves from "../../../components/Waves/Waves";
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import ScrollToTop from "../../../components/ScrollToTop/ScrollToTop";
+import Data from "../../../textComp/join/ms/Data";
 
+const StudentsJoin = () => {
+    return (
+        <React.Fragment>
+            <Header />
+            <StartTop
+                text="Prospective MS Students"
+                // image="/204c420e096e4706ee89dd37031dc33e.jpg"
+                image="/img2.png"
+            />
+            <Waves r={255} g={255} b={255} />
+            <ScrollToTop />
+            <Data />
+            <Footer />
+        </React.Fragment>
+    );
+};
+
+export default StudentsJoin;
