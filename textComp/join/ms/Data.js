a61ff8bb1e7155b04d4fa45252791640e3056eec
Mohit Kataria <mohit.kataria8860@gmail.com>
2023-04-25 15:25:42 +0530

    Update Data.js

--- textComp/join/ms/Data.js
+++ textComp/join/ms/Data.js
@@ -54,10 +54,7 @@ const Data = () => {
                         </a>{" "}
                         for any clarification.
                     </p>
-                    //<p><strong>New: Provisional list of selected candidates for MSR and PhD program in Summer 2022-23 : {" "}<a href="/pdf/Final_list_MSR_PhD_Summer_2022.pdf" target="_blank" rel="noopener noreferrer" >click here</a></strong>
-                    //</p>
-                    //<p><strong>New: Provisional list of selected candidates for MSR and PhD program in Winter 2022-23 : {" "}<a href="/pdf/Final_list_MSR_PhD_Winter_2022.pdf" target="_blank" rel="noopener noreferrer" >click here</a></strong>
-                    //</p>
+                   
                 </div>
             </div>
         </>
